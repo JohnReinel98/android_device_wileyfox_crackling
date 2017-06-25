a72dc1d89fbe1ab0ad00adcb8217401467c830af
ProtoHype98 <johnrenielcaluag@yahoo.com>
2017-06-25 10:32:21 +0800

    DND bringup

--- board/init.mk
+++
@@ -1,3 +0,0 @@
-# A6000 init
-TARGET_LIBINIT_MSM8916_DEFINES_FILE := device/wileyfox/crackling/init/init_crackling.cpp
-TARGET_UNIFIED_DEVICE := true
--- dnd.mk
+++ dnd.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2015 The CyanogenMod Project
+# Copyright 2017 The DarkNess reDefined Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,40 +15,26 @@
 
 $(call inherit-product, device/wileyfox/crackling/full_crackling.mk)
 
-# Inherit some common XenonHD stuff.
-$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+# Inherit some common DND stuff.
+$(call inherit-product, vendor/dnd/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := xenonhd_crackling
+PRODUCT_NAME := dnd_crackling
 BOARD_VENDOR := wileyfox
+PRODUCT_DEVICE := crackling
 
 PRODUCT_GMS_CLIENTID_BASE := android-wileyfox
 
 PRODUCT_MANUFACTURER := Wileyfox
+PRODUCT_MODEL := Wileyfox Swift
 
 PRODUCT_BRAND := Wileyfox
 TARGET_VENDOR := wileyfox
 TARGET_VENDOR_PRODUCT_NAME := Swift
 TARGET_VENDOR_DEVICE_NAME := crackling
 
-PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=Lordhmen
-
-PRODUCT_PACKAGES += \
-    AdAway \
-    KernelAdiutor \
-    MiXplorer
-
-# Root options
-ROOT_METHOD=magisk
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := crackling
-
-# Use the latest approved GMS identifiers unless running a signed build
-ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=Wileyfox/Swift/crackling:7.1.1/N6F26Y/5506d7d322:user/release-keys \
     PRIVATE_BUILD_DESC="crackling-user 7.1.1 N6F26Y 5506d7d322 release-keys"
-endif	
